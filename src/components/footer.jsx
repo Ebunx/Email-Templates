afa0b48c7d0a25919557ab6629cb5015d5507c4d
Ebunoluwa Owoeye <ebunowoeye316@gmail.com>
2025-07-18 10:39:48 +0100

    add vercel configuration for URL rewrites

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,16 +3,18 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <footer class="text-center mt-5 pb-3 place-content-center">
+    <footer className="flex flex-col items-center mt-5 pb-3">
+  <img
+    src="HOSTBRIDGE.png"
+    alt="HostBridge Logo"
+    className="w-[50%] md:w-[30%]"
+  />
+  <p className="text-center mt-2">A Hosting Company</p>
+  <p className="text-center">123 Creative Street,</p>
+  <p className="text-center">San Francisco</p>
+  <p className="text-center">All rights reserved · Made with ❤</p>
+</footer>
 
-          <img src="HOSTBRIDGE.png" alt="" width="300 " class="place-self-center w-[50%] md:w-[30%] "/>
-              <p class="text-center md:w-[30%]  place-self-center mt-2 "> A Hosting Company</p>
-
-               <p class="text-center md:w-[30%]  place-self-center mt-2 ">123 Creative street, </p>
-    <p class="text-center md:w-[30%]  place-self-center ">  San Francisco   </p>
-    <p class="text-center md:w-[30%]  place-self-center "> All rights reserved Made with ❤ </p>
-      
-    </footer>
 
  
 </> 
